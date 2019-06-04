c5468e2a668d3f74d7dd7ec963ef24bc8066f453
trongphuclhu <trongphuclhu@gmail.com>
2019-06-05 00:11:21 +0700

    no message

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace WebASPNETCore.Models
 {
     public class ErrorViewModel
     {
-		//Nhanh phuc code moi
+		//Nhanh Master code moi
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
